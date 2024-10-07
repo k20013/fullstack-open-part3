2a194e6d6ada3fa68dfc2ffb4997f3b278f5db6f
CalebBravo <calebphone72@gmail.com>
2024-10-07 12:46:13 -0300

    3.4: Backend de la Agenda Telefónica, paso 4

--- index.js
+++ index.js
@@ -2,34 +2,34 @@ const express = require('express');
 const app = express();
 
 const persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
+    {
+        "id": 1,
+        "name": "Arto Hellas",
+        "number": "040-123456"
     },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
+    {
+        "id": 2,
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523"
     },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
+    {
+        "id": 3,
+        "name": "Dan Abramov",
+        "number": "12-43-234345"
     },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
+    {
+        "id": 4,
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122"
     }
 ];
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
+    res.send('<h1>Hello World!</h1>')
 });
 
 app.get('/api/persons', (req, res) => {
-  res.json(persons)
+    res.json(persons)
 });
 
 app.get('/info', (req, res) => {
@@ -39,7 +39,7 @@ app.get('/info', (req, res) => {
         <p>Phonebook has info for ${persons.length} people</p>
         <p>${currentTime.toLocaleString()}</p>
         `
-    ) 
+    )
 });
 
 app.get('/api/persons/:id', (req, res) => {
@@ -47,11 +47,17 @@ app.get('/api/persons/:id', (req, res) => {
         element => element.id === Number(req.params.id)
     );
     wanted
-    ? res.json(wanted)
-    : res.status(404).end()
+        ? res.json(wanted)
+        : res.status(404).end()
+});
+
+app.delete('/api/persons/:id', (req, res) => {
+    const target = persons.find(element => element.id === Number(req.params.id));
+    target && persons.splice(persons.indexOf(target), 1);
+    res.status(204).end();
 });
 
 const PORT = 3001;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+    console.log(`Server running on port ${PORT}`)
 });
