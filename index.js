a3b2f928517fdaf44ef1a9cb83f3455a718cdbd7
CalebBravo <calebphone72@gmail.com>
2024-10-26 14:22:31 -0300

    3.15: Base de datos de la Agenda Telefónica, paso 3

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.get('/api/persons/:id', (req, res) => {
 });
 
 app.delete('/api/persons/:id', (req, res) => {
-    Person.deleteOne({ _id: req.params.id }).then(
+    Person.findByIdAndDelete(req.params.id).then(
         () => res.status(204).end()
     )
 });
