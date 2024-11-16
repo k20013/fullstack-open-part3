e334f4a3b4c2ed74ca6326318ceffe1579b92a46
CalebBravo <calebphone72@gmail.com>
2024-11-16 14:28:38 -0300

    3.22: Configuración de Lint

---
+++ eslint.config.mjs
@@ -0,0 +1,36 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import stylisticJs from '@stylistic/eslint-plugin-js';
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
+  { languageOptions: { globals: globals.node } },
+  pluginJs.configs.recommended,
+  { ignores: ['dist'] },
+  {
+    plugins: {
+      '@stylistic/js': stylisticJs
+    },
+    rules: {
+      '@stylistic/js/indent': [
+        'error',
+        2
+      ],
+      '@stylistic/js/quotes': [
+        'error',
+        'single'
+      ],
+      'eqeqeq': 'error',
+      'no-trailing-spaces': 'error',
+      'object-curly-spacing': [
+        'error', 'always'
+      ],
+      'arrow-spacing': [
+        'error', { 'before': true, 'after': true },
+      ],
+      'no-console': 'off',
+    }
+  }
+];
--- index.js
+++ index.js
@@ -2,24 +2,24 @@ require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
-const static = require('static');
+// const static = require('static');
 
 const app = express();
 const Person = require('./models/person');
 
-morgan.token('body', (req, res) => JSON.stringify(req.body));
+morgan.token('body', (req,) => JSON.stringify(req.body));
 
-const errorHandler = (error, req, res, next) => {
-    console.error(error);
+const errorHandler = (error, res, next) => {
+  console.error(error);
 
-    if (error.name === 'CastError') {
-        return res.status(400).send({ error: 'malformatted id' });
-    }
-    else if (error.name === 'ValidationError') {
-        return res.status(400).json({ error: error._message })
-    }
+  if (error.name === 'CastError') {
+    return res.status(400).send({ error: 'malformatted id' });
+  }
+  else if (error.name === 'ValidationError') {
+    return res.status(400).json({ error: error._message })
+  }
 
-    next(error);
+  next(error);
 }
 
 app.use(express.json());
@@ -27,60 +27,60 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :b
 app.use(cors());
 app.use(express.static('dist'));
 
-app.get('/api/persons', (req, res, next) => {
-    Person.find({}).then(p => {
-        res.json(p);
-    });
+app.get('/api/persons', (req, res) => {
+  Person.find({}).then(p => {
+    res.json(p);
+  });
 });
 
-app.get('/info', (req, res, next) => {
-    const currentTime = new Date();
-    Person.find({}).then(p => {
-        res.send(
-            `<p>Phonebook has info for ${p.length} people</p><p>${currentTime.toLocaleString()}</p>`
-        )
-    });
+app.get('/info', (req, res) => {
+  const currentTime = new Date();
+  Person.find({}).then(p => {
+    res.send(
+      `<p>Phonebook has info for ${p.length} people</p><p>${currentTime.toLocaleString()}</p>`
+    )
+  });
 });
 
 app.get('/api/persons/:id', (req, res, next) => {
-    Person.findById(req.params.id).then(p => {
-        p ?
-            res.status(200).json(p)
-            : res.status(404).end();
-    })
-        .catch(error => next(error));
+  Person.findById(req.params.id).then(p => {
+    p ?
+      res.status(200).json(p)
+      : res.status(404).end();
+  })
+    .catch(error => next(error));
 });
 
 app.delete('/api/persons/:id', (req, res, next) => {
-    Person.findByIdAndDelete(req.params.id).then(
-        () => res.status(204).end()
-    )
-        .catch(error => next(error));
+  Person.findByIdAndDelete(req.params.id).then(
+    () => res.status(204).end()
+  )
+    .catch(error => next(error));
 });
 
 app.post('/api/persons', (req, res, next) => {
-    if (!req.body.name || !req.body.number) return res.status(400).json({ error: "content missing" });
-    const person = new Person({
-        name: req.body.name,
-        number: req.body.number
-    });
-    person.save().then(added => res.status(200).json(added))
-        .catch(error => next(error));
+  if (!req.body.name || !req.body.number) return res.status(400).json({ error: 'content missing' });
+  const person = new Person({
+    name: req.body.name,
+    number: req.body.number
+  });
+  person.save().then(added => res.status(200).json(added))
+    .catch(error => next(error));
 });
 
 app.put('/api/persons/:id', (req, res, next) => {
 
-    Person.findByIdAndUpdate(req.params.id, {
-        name: req.body.name,
-        number: req.body.number
-    }, { runValidators: true }).then(
-        () => res.status(200).json(req.body)
-    ).catch(error => next(error));
+  Person.findByIdAndUpdate(req.params.id, {
+    name: req.body.name,
+    number: req.body.number
+  }, { runValidators: true }).then(
+    () => res.status(200).json(req.body)
+  ).catch(error => next(error));
 })
 
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 });
